fae1fc94f9292d18f0700f45786cca99699be97e
ulsandonghun <donghoon141@naver.com>
2024-01-25 20:18:29 +0900

    [setting] : 초기 세팅

---
+++ src/main/java/Donghun/JPAShoppingMall/JpaShoppingMallApplication.java
@@ -0,0 +1,13 @@
+package Donghun.JPAShoppingMall;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class JpaShoppingMallApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(JpaShoppingMallApplication.class, args);
+	}
+
+}
---
+++ src/test/java/Donghun/JPAShoppingMall/JpaShoppingMallApplicationTests.java
@@ -0,0 +1,13 @@
+package Donghun.JPAShoppingMall;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class JpaShoppingMallApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
